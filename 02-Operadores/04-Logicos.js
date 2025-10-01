d8b646cf499692a2a2aa58a213bf492c7bf5d15d
SebaJeldres <Sjeldresosoriodev@gmail.com>
2025-10-01 17:26:30 -0300

    Agregar archivo 04-Logicos.js con ejemplos de operadores lógicos y actualizar script en index.html

---
+++ 02-Operadores/04-Logicos.js
@@ -0,0 +1,30 @@
+// and or not
+
+
+
+// and -> &&
+
+console.log(true && true); // true
+console.log(true && false);
+
+// or -> ||
+
+console.log(true || false); // true
+console.log(false || false); // false
+
+// not -> !
+console.log(!true); // false
+console.log(!false); // true
+
+
+// ejemplos
+console.log(!false && true); // true
+console.log(!(false && true));
+console.log(!false || false); // true
+console.log(!(false || false)); // true
+console.log(!false || !true); // true
+console.log(!(false || true)); // false
+console.log(!false && !true); // false
+
+
+
