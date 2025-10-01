816748f34128245243d63b23fcf3d106a162aae5
SebaJeldres <Sjeldresosoriodev@gmail.com>
2025-10-01 17:46:43 -0300

    Actualizar script en index.html y agregar ejemplos de control de flujo con if y else

---
+++ 02-Operadores/08-ternario.js
@@ -0,0 +1,11 @@
+// expresion ? "si es true" : "si es false"
+
+let edad = 18;
+
+let acceso = edad > 17? "puede entrar" : "no puede entrar";
+
+console.log(acceso);
+
+
+// let acceso;
+
---
+++ 03-Control_flujo/02-else.js
@@ -0,0 +1,13 @@
+let nombre = "Sebastian";
+let edad = 21;
+
+
+if (edad >= 18) {
+    console.log(nombre + " es mayor de edad");
+    
+} else if (edad > 13) {
+    console.log(nombre + " es un adolescente");
+
+} else {
+    console.log(nombre + " es menor de edad");
+}
